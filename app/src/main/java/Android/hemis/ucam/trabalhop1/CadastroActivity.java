01f36660df7be97963c2b9801874e865ded053dc
Rhemisson <rhemissonnascimento@gmail.com>
2016-10-19 00:54:08 -0200

    FinalizadoProjeto

---
+++ app/src/main/java/Android/hemis/ucam/trabalhop1/CadastroActivity.java
@@ -0,0 +1,47 @@
+package Android.hemis.ucam.trabalhop1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private EditText matricula;
+    private EditText email;
+    private EditText contaGit;
+    private Button cadastrar;
+    private Button limpar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        nome = (EditText) findViewById(R.id.editTextCadNome);
+        matricula = (EditText) findViewById(R.id.editTextCadMatricula);
+        email = (EditText) findViewById(R.id.editTextCadEmail);
+        contaGit = (EditText) findViewById(R.id.editTextCadConta);
+        cadastrar = (Button) findViewById(R.id.buttonCadCadastrar);
+        limpar = (Button) findViewById(R.id.buttonCadLimpar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "Cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        limpar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                nome.setText("");
+                matricula.setText("");
+                email.setText("");
+                contaGit.setText("");
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/Android/hemis/ucam/trabalhop1/PrincipalActivity.java
@@ -0,0 +1,52 @@
+package Android.hemis.ucam.trabalhop1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText senha;
+    private Button acessar;
+    private Button limpar;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        email = (EditText) findViewById(R.id.editTextEmail);
+        senha = (EditText) findViewById(R.id.editTextSenha);
+        acessar = (Button) findViewById(R.id.buttonAcessar);
+        limpar = (Button) findViewById(R.id.buttonLimpar);
+        resultado = (TextView) findViewById(R.id.textViewResultado);
+
+        acessar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(email.getText().toString().equalsIgnoreCase("admin@admin.com") && senha.getText().toString().equalsIgnoreCase("12345"))
+                {
+                    Intent trocadorTela = new Intent(PrincipalActivity.this, TarefaActivity.class);
+                    startActivity(trocadorTela);
+                }else{
+                    resultado.setText("Usuário ou senha inválidos.");
+                }
+            }
+        });
+
+        limpar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                email.setText("");
+                senha.setText("");
+                resultado.setText("");
+            }
+        });
+    }
+}
